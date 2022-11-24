f99e523ebfcc4169620e2336330cb9aeb46087e5
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2022-11-25 01:41:24 +0800

    shag

---
+++ SoftUniJS/Arrays/NestedArray/Looping.js
@@ -0,0 +1,10 @@
+let arr = [[4, 5, 6],
+           [6, 5, 4],
+           [5, 5, 5]];
+
+arr.forEach(printRow);
+
+function printRow(row){
+    console.log(row);
+    row.forEach(el => console.log(el));
+}
